f2ea310f9f77bbf8f2c0bc6a11ee4265285b98e0
Diogo <diogo1243@gmail.com>
2019-06-15 21:08:31 -0300

    Jogador já seleciona a "melhor" jogada e retorna, como resposta, o tabuleiro que vai ficar calculada a resposta.

--- jogador.py
+++ jogador.py
@@ -10,11 +10,14 @@ from keras.layers import Dense
 from keras import initializers
 import numpy as np
 from random import randint
-
+from gerenciadorDeTabuleiros import GerenciadorDeTabuleiros
+import copy
 
 class Jogador:
     
-    stddevJogador = 0.05
+    stddevJogador = 5
+    numeroJogadasAFrente = 2
+    numeroDePossiveisComidasParaNaoConsiderarJogadaForcada = 3
     
     def __init__ (self):
         self.valorDama = (randint(10,30) /10.0)
@@ -31,3 +34,57 @@ class Jogador:
         if (tabuleiro.ndim == 1):
             tabuleiro = np.array ([tabuleiro])
         return self.model.predict (tabuleiro)
+        
+    def calculaScoreTabuleiro (self, tabuleiro, numeroDaJogada):
+        if (numeroDaJogada == self.numeroJogadasAFrente):
+            return 0.0
+        
+        jogadaForcada = False
+        
+        gerenciadorDeTabuleiros = GerenciadorDeTabuleiros (tabuleiro)
+        listaTabuleiros = gerenciadorDeTabuleiros.calculaPossibilidadesDeMultiplasComidas ()
+        if (not listaTabuleiros or listaTabuleiros is None):
+            listaTabuleiros = gerenciadorDeTabuleiros.calculaPossibilidadesDeMovimentoNormal ()
+        elif (len (listaTabuleiros) < self.numeroDePossiveisComidasParaNaoConsiderarJogadaForcada):
+            jogadaForcada = True
+        
+        maxScore = -9999999999
+        
+        numeroDaProximaJogada = numeroDaJogada
+        if (not jogadaForcada):
+            numeroDaProximaJogada += 1
+            
+        for tabuleiro in listaTabuleiros:
+            if (not tabuleiro is None):
+                score = (self.predict (tabuleiro.converteTabuleiroParaArray (self.valorDama)) + self.calculaScoreTabuleiro (tabuleiro, numeroDaProximaJogada)) / 2.0
+                if (score > maxScore and numeroDaJogada == 0):
+                    maxScore = score
+        
+        return maxScore
+    
+    def selecionaMelhorJogada (self, tabuleiro, numeroDaJogada):
+        jogadaForcada = False
+        
+        gerenciadorDeTabuleiros = GerenciadorDeTabuleiros (tabuleiro)
+        listaTabuleiros = gerenciadorDeTabuleiros.calculaPossibilidadesDeMultiplasComidas ()
+        if (not listaTabuleiros or listaTabuleiros is None):
+            listaTabuleiros = gerenciadorDeTabuleiros.calculaPossibilidadesDeMovimentoNormal ()
+        elif (len (listaTabuleiros) < self.numeroDePossiveisComidasParaNaoConsiderarJogadaForcada):
+            jogadaForcada = True
+        
+        maxScore = -9999999999
+        
+        numeroDaProximaJogada = numeroDaJogada
+        if (not jogadaForcada):
+            numeroDaProximaJogada += 1
+            
+        for tabuleiro in listaTabuleiros:
+            if (not tabuleiro is None):
+                score = (self.predict (tabuleiro.converteTabuleiroParaArray (self.valorDama)) + self.calculaScoreTabuleiro (tabuleiro, numeroDaProximaJogada)) / 2.0
+                if (score > maxScore and numeroDaJogada == 0):
+#                    print ("SCORE MAIOR: " + str(score))
+#                    tabuleiro.printaTabuleiro ()
+                    maxScore = score
+                    tabuleiroEscolhido = copy.deepcopy (tabuleiro)
+                    
+        return tabuleiroEscolhido
--- tester.py
+++ tester.py
@@ -14,37 +14,31 @@ from jogador import Jogador
 
 jogador1 = Jogador ()
 
-tabuleiro = Tabuleiro (VariaveisGlobais.TABULEIRO_TESTE)
-
-print (jogador1.predict (tabuleiro.converteTabuleiroParaArray (jogador1.valorDama)))
-
-casaInicial = Casa ("a", 2)
-casaFinal = Casa ("c", 4)
-movimento1 = Movimento (casaInicial, casaFinal)
-
-casaInicial = Casa ("c", 4)
-casaFinal = Casa ("e", 6)
-movimento2 = Movimento (casaInicial, casaFinal)
-
-#listaMovimentos = []
-#listaMovimentos.append (movimento1)
-#listaMovimentos.append (movimento2)
-
-tabuleiro.printaTabuleiro ()
-
-#tabuleiro.doMultipleMovementsComer (listaMovimentos)
-
+tabuleiro = Tabuleiro (VariaveisGlobais.TABULEIRO_INICIAL)
 #tabuleiro.printaTabuleiro ()
 
+tabuleiro2 = jogador1.selecionaMelhorJogada (tabuleiro, 0)
+tabuleiro2.printaTabuleiro ()
 
-GerenciadorDeTabuleiros = GerenciadorDeTabuleiros (tabuleiro)
-listTabuleirosPossiveis = GerenciadorDeTabuleiros.calculaPossibilidadesDeMultiplasComidas ()
+#print (jogador1.predict (tabuleiro.converteTabuleiroParaArray (jogador1.valorDama)))
 #
+#casaInicial = Casa ("a", 2)
+#casaFinal = Casa ("c", 4)
+#movimento1 = Movimento (casaInicial, casaFinal)
 #
-if (not listTabuleirosPossiveis is None):
-    for tabuleiro in listTabuleirosPossiveis:
-        if (not tabuleiro is None):
-            print (jogador1.predict (tabuleiro.converteTabuleiroParaArray (jogador1.valorDama)))
-            tabuleiro.printaTabuleiro ()
-else:
-    print ("Nao existem movimentos!!")
+#casaInicial = Casa ("c", 4)
+#casaFinal = Casa ("e", 6)
+#movimento2 = Movimento (casaInicial, casaFinal)
+#
+#tabuleiro.printaTabuleiro ()
+#
+#GerenciadorDeTabuleiros = GerenciadorDeTabuleiros (tabuleiro)
+#listTabuleirosPossiveis = GerenciadorDeTabuleiros.calculaPossibilidadesDeMultiplasComidas ()
+#
+#if (not listTabuleirosPossiveis is None):
+#    for tabuleiro in listTabuleirosPossiveis:
+#        if (not tabuleiro is None):
+#            print (jogador1.predict (tabuleiro.converteTabuleiroParaArray (jogador1.valorDama)))
+#            tabuleiro.printaTabuleiro ()
+#else:
+#    print ("Nao existem movimentos!!")
